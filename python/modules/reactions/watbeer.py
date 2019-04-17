7c4592fc2c68e0598fda16f02eb5de6bc00b091c
Niels Kamp <n.kamp@developers.nl>
2019-04-17 09:38:12 +0200

    small injoke

---
+++ python/modules/reactions/watbeer.py
@@ -0,0 +1,20 @@
+
+RACCOON_EMOJI_NAME = "WAT"
+EMOJI_ID = 539759475930955787
+
+raccoon_words = ["raccoon", "wasbeer", "wasberen", "watbeer"]
+
+
+def is_raccoon_emoji(emoji):
+    if emoji.id == EMOJI_ID:
+        return True
+    return False
+
+
+def check_for_raccoon(message):
+    if RACCOON_EMOJI_NAME in message:
+        return [RACCOON_EMOJI_NAME]
+    for word in raccoon_words:
+        if word in message.lower():
+            return [RACCOON_EMOJI_NAME]
+    return []
