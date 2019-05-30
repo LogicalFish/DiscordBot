751f7c76d4f5f332d2221fa78f737ce4a1e57fc0
Niels Kamp <niels.kamp@xs4all.nl>
2019-05-30 22:13:51 +0200

    Example for list_config

---
+++ python/modules/characterlist/list_config_example.py
@@ -0,0 +1,9 @@
+import os
+import config
+
+DATA_DIR = os.path.sep.join([config.BASEDIR, "modules", "characterlist", "data"])
+NPC_LIST = os.path.sep.join([DATA_DIR, "NPCs.xml"])
+
+BASE_YEAR = 2000  # Insert the current year here.
+
+OWNER_ID = -1  # Insert your own ID here.
