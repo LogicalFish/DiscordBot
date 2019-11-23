a4b54471e501bf57740fac6570636c2104a32233
Niels Kamp <niels.kamp@xs4all.nl>
2019-11-23 17:05:36 +0100

    Removed commented-out code

--- python/commands/__init__.py
+++ python/commands/__init__.py
@@ -12,8 +12,6 @@ from modules.dice.godroll_command import GodRollCommand
 from modules.games.minesweeper.minesweeper_command import MineSweeperCommand
 from modules.games.tictactoe.ttt_commands import ChallengeCommand, PlayGameCommand, AbandonGameCommand
 from modules.games.wheel.wheel_main_command import MainWheelCommand
-# from modules.games.wheel.wheel_command import JoinWheelCommand, SpinWheelCommand, GuessConsonantCommand, \
-#     BuyVowelCommand, SolveCommand, WheelStatusCommand, WheelQuitCommand, WheelScoreCommand
 from modules.nicknames.nickname_command import CallmeCommand
 from modules.reminders.reminder_command import RemindCommand
 
@@ -27,8 +25,6 @@ commands_list = [CallmeCommand(), EchoCommand(), PollCommand(),
                  EventCommand(), ListEventCommand(), CreateEventCommand(), EditEventCommand(), DeleteEventCommand(),
                  UnShadowCommand(), RemindCommand(),
                  ListAllCommand(), WhoIsCommand(), GetYearCommand(), AddYearCommand(),
-                 # JoinWheelCommand(), SpinWheelCommand(), GuessConsonantCommand(), BuyVowelCommand(), SolveCommand(),
-                 # WheelStatusCommand(), WheelQuitCommand(), WheelScoreCommand(),
                  MainWheelCommand()]
 
 MainCommand = CommandRunner(commands_list)
