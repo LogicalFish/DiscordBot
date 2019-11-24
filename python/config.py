c8c728aa542fa7030144d499f908a39fa550b080
Niels Kamp <niels.kamp@xs4all.nl>
2019-11-24 11:42:16 +0100

    Removed useless variable

--- python/config.py
+++ python/config.py
@@ -4,6 +4,5 @@ import yaml
 configuration = yaml.safe_load(open("settings.yml"))
 
 # Directories and Files
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
 DB_INI = os.path.sep.join(configuration['dir']['database_file'])
 ERRORS = os.path.sep.join(configuration['dir']['errors'])
