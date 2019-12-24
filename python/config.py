78128b6cc726cf3a0dfa6dd5bf1f66f2eaa9e0cb
Niels Kamp <niels.kamp@xs4all.nl>
2019-12-24 19:45:48 +0100

    Changed more Relative Paths to static

--- python/config.py
+++ python/config.py
@@ -3,6 +3,7 @@ import yaml
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
-configuration = yaml.safe_load(open("settings.yaml"))
-localization_file = os.path.sep.join(configuration['dir']['localization'])
+configuration_file = os.path.sep.join([BASEDIR, "settings.yaml"])
+configuration = yaml.safe_load(open(configuration_file))
+localization_file = os.path.sep.join([BASEDIR] + configuration['dir']['localization'])
 localization = yaml.safe_load(open(localization_file, encoding="utf8"))
