7224695f2b2d878caeccba64f3132ba64e4ed604
Niels Kamp <niels.kamp@xs4all.nl>
2019-12-24 20:17:50 +0100

    Shrunk Minimal_settings some more.

--- python/database/models/__init__.py
+++ python/database/models/__init__.py
@@ -1,9 +1,12 @@
-from database.models.banned_channels_model import BannedChannel
-from database.models.birthday_model import Birthday
-from database.models.nicknames_model import Nickname
 from config import configuration
 
-models = [BannedChannel, Birthday, Nickname]
+models = []
+
+if configuration['database']['active']:
+    from database.models.banned_channels_model import BannedChannel
+    from database.models.birthday_model import Birthday
+    from database.models.nicknames_model import Nickname
+    models = [BannedChannel, Nickname, Birthday]
 
 if configuration['commands']['calendar']:
     from database.models.event_model import Event
