f6c7a740b862c450a736b49f0a922cc6e92ace88
Niels Kamp <niels.kamp@xs4all.nl>
2019-12-27 17:59:25 +0100

    Moved Birthday Channels to Settings + Models Fix

--- python/bot_tasks.py
+++ python/bot_tasks.py
@@ -1,11 +1,12 @@
 import discord
 from discord.utils import get
 import asyncio
+import logging
 
 import config
 
 THIRD_DAY = 8*60*60
-main_channel_names = ["main", "general"]
+logger = logging.getLogger(__name__)
 
 """
 Various methods that set up additional loops running in the background.
@@ -18,6 +19,7 @@ async def calendar_task(client, system):
     """
     await client.wait_until_ready()
     while not client.is_closed() and system.time_manager is not None:
+        logger.debug("Checking the calendar for reminders.")
         reminders = system.time_manager.clock_pass()
         for date, reminder_embed, channel_name, tag_name in reminders:
             if len(channel_name):
@@ -39,6 +41,7 @@ async def birthday_task(client, system):
     """
     await client.wait_until_ready()
     while not client.is_closed():
+        logger.debug("Checking the Birthday Manager for birthdays.")
         birthday_ids = system.birthday_manager.get_today_birthdays()
         for birthday_id in birthday_ids:
             channels = get_main_channels(client)
@@ -59,6 +62,7 @@ async def reminder_task(client, system):
     """
     await client.wait_until_ready()
     while not client.is_closed() and system.reminder_manager is not None:
+        logger.debug("Checking the Reminder Manager for reminders.")
         reminder_list = system.reminder_manager.get_all_reminders()
         for reminder in reminder_list:
             date, message, owner = reminder
@@ -66,9 +70,9 @@ async def reminder_task(client, system):
         await asyncio.sleep(60)
 
 
-def get_main_channels(client):
+def get_main_channels(client, system):
     main_channels = []
-    for channel_name in main_channel_names:
+    for channel_name in system.configuration['birthdays']['channels']:
         named_channels = filter(lambda ch: ch.name == channel_name, client.get_all_channels())
         for channel in named_channels:
             if isinstance(channel, discord.TextChannel):
